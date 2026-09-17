423f97bb0fad6d9d029b62a0c5b238b3298177b4
bybrave <opmybrave@gmail.com>
2026-09-17 23:44:40 +0200

    refactor: import package.json with import attributes in build/package.js

--- build/package.js
+++ build/package.js
@@ -1,11 +1,9 @@
 import fs from 'node:fs'
 import {fileURLToPath} from 'node:url'
 import * as JSON5 from '../lib/index.js'
-
-const root = new URL('../', import.meta.url)
-const pkg = JSON.parse(fs.readFileSync(new URL('package.json', root), 'utf8'))
+import pkg from '../package.json' with {type: 'json'}
 
 let pkg5 = '// This is a generated file. Do not edit.\n'
 pkg5 += JSON5.stringify(pkg, null, 2)
 
-fs.writeFileSync(fileURLToPath(new URL('package.json5', root)), pkg5)
+fs.writeFileSync(fileURLToPath(new URL('../package.json5', import.meta.url)), pkg5)
